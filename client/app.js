67179a9416e0c4a84e32b263f0ee72bbb978bb12
Victor Huynh <victor.h.huynh@gmail.com>
2019-08-19 22:10:03 +0000

    Completed base functionality

--- client/app.js
+++ client/app.js
@@ -1,5 +1,23 @@
-$('form').on('submit',function(){
+var socket = io();
+
+$('form').on('submit',function() {
   var text = $('#message').val();
-  alert(text);
+  socket.emit('message', text);
+    $('#message').val('');
   return false;
 });
+
+$('form').on('submit',function() {
+  var text = $('#initials').val();
+  socket.emit('initials', text);
+    $('#initials').val('');
+    return false;
+})
+
+socket.on('message', function(msg) {
+  $('<li>').text(msg).appendTo('#history');
+})
+
+socket.on('initials', function(msg) {
+  $('<li>').text(msg).appendTo('#history')
+})
--- server.js
+++ server.js
@@ -4,8 +4,16 @@ var app = express();
 var http = require('http');
 var server = http.Server(app);
 
+var io = require('socket.io')(server);
+
 app.use(express.static('client'));
 
 server.listen(8080, function() {
   console.log('Chat server running');
 })
+
+io.on('connection', function(socket) {
+  socket.on('message', function(msg) {
+    io.emit('message', msg);
+  });
+});
